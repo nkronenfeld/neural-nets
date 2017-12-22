1feedff0b89e62bf32b38a9789de47249c51fca9
Nathan Kronenfeld <nicole.oresme@gmail.com>
2017-12-22 05:56:55 -0500

    Matrix inversion

--- src/main/scala/ndk/math/linalg/Matrix.scala
+++ src/main/scala/ndk/math/linalg/Matrix.scala
@@ -7,6 +7,12 @@ import scala.util.Random
 // A matrix, a listing of C columns of length R (i.e., column-major order
 class Matrix (val C: Int, val R: Int, val values: Array[Double]) {
   assert(R * C == values.length)
+  def row (r: Int): Vector =
+    new Vector(values.grouped(R).map(_(r)).toArray)
+
+  def column(c: Int): Vector =
+    new Vector(values.slice(c * R, (c + 1) * R))
+
   def apply (c: Int, r: Int): Double = {
     assert(0 <= c && c < C)
     assert(0 <= r && r < R)
@@ -73,9 +79,22 @@ class Matrix (val C: Int, val R: Int, val values: Array[Double]) {
         (0 until (C * R)).map(n => this.values(n) == that.values(n)).reduce(_ && _)
     case _ => false
   }
+  def approximatelyEquals (epsilon: Double)(other: Any): Boolean = other match {
+    case that: Matrix =>
+      this.C == that.C && this.R == that.R &&
+        (0 until (C * R)).map { n =>
+          val a = this.values(n)
+          val b = that.values(n)
+          if (a.abs < epsilon) b.abs < epsilon
+          else (1.0 - epsilon < a / b && a / b < 1.0 + epsilon)
+        }.reduce(_ && _)
+    case _ => false
+  }
 
-  override def toString: String = s"Matrix[C=${C} x R=${R}]"
-}
+  override def toString: String = s"Matrix[C=${C} x R=${R}]: " + toStringRows.mkString("")
+  def toStringRows: Seq[String] =
+    (0 until R).map(r => row(r).values.map(n => f"${n}%2.2f").mkString("| ", " ", " |"))
+ }
 
 object Matrix {
   def fromArrays (columns: Array[Double]*): Matrix = {
@@ -99,5 +118,54 @@ object Matrix {
 
   def zero (c: Int, r: Int): Matrix = {
     new Matrix(c, r, Array.fill(c * r)(0.0))
+  }
+
+  def one (n: Int): Matrix = {
+    val rowPlusOne = 1.0 +: Array.fill(n)(0.0)
+    new Matrix(n, n, Array.fill(n-1)(rowPlusOne).flatten :+ 1.0)
+  }
+
+  def invertSquareMatrix (M: Matrix): Matrix = {
+    assert(M.C == M.R)
+    val N = M.C
+    val epsilon = 1E-6
+
+    // Get the MI pair on which to perform the inversion
+    val inversionSpace = new Array[Array[Double]](N)
+    inversionSpace.indices.foreach { n =>
+      inversionSpace(n) = M.row(n).values ++ Array.fill(N)(0.0)
+      inversionSpace(n)(N + n) = 1.0
+    }
+
+    def swapRows (a: Int, b: Int): Unit = {
+      val tmp = inversionSpace(a)
+      inversionSpace(a) = inversionSpace(b)
+      inversionSpace(b) = tmp
+    }
+    def scaleRow (a: Int, scale: Double): Unit = {
+      (0 until N*2).foreach(n => inversionSpace(a)(n) = inversionSpace(a)(n) * scale)
+    }
+    def addScaledRow (a: Int, b: Int, scale: Double): Unit = {
+      (0 until N*2).foreach(n =>
+        inversionSpace(a)(n) = inversionSpace(a)(n) + inversionSpace(b)(n) * scale
+      )
+    }
+
+    (0 until N).foreach { n =>
+      // Find a row with a non-0 nth entry
+      val r = (n until N).find(nn => inversionSpace(nn)(n).abs > epsilon)
+      // Swap this row to the right position
+      r.map(rr => if (n != rr) swapRows(n, rr)).orElse(throw new Exception("Uninvertable matrix"))
+      // Scale it so that the nth entry is 1.0
+      scaleRow(n, 1.0 / inversionSpace(n)(n))
+      // Remove the nth entry from all other rows
+      (0 until N).map { nn =>
+        if (n != nn) {
+          addScaledRow(nn, n, -inversionSpace(nn)(n))
+        }
+      }
+    }
+
+    new Matrix(N, N, inversionSpace.map(_.drop(N)).reduce(_ ++ _)).transpose()
   }
 }
--- src/test/scala/ndk/math/linalg/MatrixTests.scala
+++ src/test/scala/ndk/math/linalg/MatrixTests.scala
@@ -1,8 +1,11 @@
 package ndk.math.linalg
 
+import org.scalactic.{Equality, TolerantNumerics}
 import org.scalatest.FunSpec
 
 class MatrixTests extends FunSpec {
+  implicit val matricesEqual = new TolerantMatrixEquality(1E-12)
+
   describe("Multiplication") {
     it("should multiply a vector properly") {
       val A = Matrix.fromArrays(Array(1.0, 4.0, 2.0), Array(-3.0, 5.0, -4.0))
@@ -30,4 +33,55 @@ class MatrixTests extends FunSpec {
       assert(b === a.transpose())
     }
   }
+  describe("Standard matrices") {
+    it("should generate a proper 2x2 zero matrix") {
+      val M = Matrix.zero(2, 2)
+      assert(0.0 === M(0, 0))
+      assert(0.0 === M(0, 1))
+      assert(0.0 === M(1, 0))
+      assert(0.0 === M(1, 1))
+    }
+    it("should generate a proper 2x2 unit matrix") {
+      val M = Matrix.one(2)
+      assert(1.0 === M(0, 0))
+      assert(0.0 === M(0, 1))
+      assert(0.0 === M(1, 0))
+      assert(1.0 === M(1, 1))
+    }
+    it("should generate a proper 3x3 unit matrix") {
+      val M = Matrix.one(3)
+      for (i <- 0 until 3; j <- 0 until 3) {
+        if (i == j) assert(1.0 === M(i, j))
+        else assert(0.0 === M(i, j))
+      }
+    }
+    it("should generate a proper 4x4 unit matrix") {
+      val M = Matrix.one(4)
+      for (i <- 0 until 4; j <- 0 until 4) {
+        if (i == j) assert(1.0 === M(i, j))
+        else assert(0.0 === M(i, j))
+      }
+    }
+  }
+  describe("Inversion") {
+    it("should invert a simple 2x2 matrix") {
+      val M = Matrix.fromArrays(Array(1.0, 3.0), Array(-2.0, 2.5))
+      val Mi = Matrix.invertSquareMatrix(M)
+      assert(Matrix.one(2) === M * Mi)
+      assert(Matrix.one(2) === Mi * M)
+    }
+    it("should throw an exception when trying to invert a non-square matrix") {
+      val M = Matrix.fromArrays(Array(1.0, 2.0, 3.0), Array(1.0, 3.0, 7.0))
+      intercept[AssertionError](Matrix.invertSquareMatrix(M))
+    }
+    it("should throw an exception when trying to invert an uninvertable matrix") {
+      val M = Matrix.fromArrays(Array(1.0, 2.0), Array(1.0, 2.0))
+      val thrown = intercept[Exception](Matrix.invertSquareMatrix(M))
+      assert("Uninvertable matrix" === thrown.getMessage)
+    }
+  }
+}
+
+class TolerantMatrixEquality (epsilon: Double) extends Equality[Matrix] {
+  override def areEqual(a: Matrix, b: Any) = a.approximatelyEquals(epsilon)(b)
 }
